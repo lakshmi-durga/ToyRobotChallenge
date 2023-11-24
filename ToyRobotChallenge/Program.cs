9891653b4903bc4f215ca219bf0ee3ae79e53cba
Kotikelapoodi, Lakshmi Durga <Lakshmi.Kotikelapoodi@ncratleos.com>
2023-11-25 04:29:31 +0530

    Initial commit

---
+++ ToyRobotChallenge/Program.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ToyRobotChallenge 
+{
+    internal class Program
+    {
+       
+        static void Main(string[] args)
+        {
+            Console.WriteLine("***********Welcome to Toy Robot Application************");
+            Console.WriteLine("The application is a simulation of a toy robot moving on a square tabletop, of dimensions 5 units x 5 units");
+            Console.WriteLine("RULES:");
+            Console.WriteLine("1. The first valid command to the robot is a PLACE command, after that, any sequence of commands may be issued, in any order, including another PLACE command");
+            Console.WriteLine("2. MOVE will move the toy robot one unit forward in the direction it is currently facing.");
+            Console.WriteLine("3. LEFT and RIGHT will rotate the robot 90 degrees in the specified direction without changing the position of the robot.");
+            Console.WriteLine("4. REPORT will announce the X,Y and F of the robot.");
+            Console.WriteLine("5. A robot that is not on the table can choose the ignore the MOVE, LEFT, RIGHT and REPORT commands.");
+            Console.WriteLine("6. EXIT will exit the application");
+            Console.WriteLine("Application takes following commands:");
+            Console.WriteLine("PLACE X,Y,F\r\nMOVE\r\nLEFT\r\nRIGHT\r\nREPORT");
+            Console.WriteLine("****************************************************");
+            ToyRobot toyRobot = new ToyRobot();
+            String input = Console.ReadLine();
+            bool firstinput = true;
+            while(input.ToUpper() != "EXIT")
+            {
+                if (firstinput)
+                {
+                    if (validateFirstInput(input))
+                    {
+                        firstinput = false;
+                        toyRobot.Command(input);
+
+                    }
+                }
+                else
+                {
+                    if (validateInput(input))
+                    {
+                        toyRobot.Command(input);
+                    }
+                }
+                input = Console.ReadLine();
+            }
+        }
+
+        internal static bool validateInput(String input)
+        {
+            bool result = true;
+            int x, y;
+            String[] inputs = input.Split(",");
+            if (inputs.Length == 3)
+            {
+                List<String> placecommad = inputs[0].Split(" ").ToList();
+                placecommad.Add(inputs[1]);
+                placecommad.Add(inputs[2]);
+                if (placecommad[0].Trim().ToUpper() == "PLACE")
+                {
+                    if (int.TryParse(placecommad[1], out x) && int.TryParse(placecommad[2], out y))
+                    {
+                        if ((x > 5 || x < 0) || (y < 0 || y > 5))
+                        {
+                            result = false;
+                            Console.WriteLine("Cannot place robot at {0} {1}", x, y);
+                        }
+                    }
+                    else
+
+                    {
+                        result = false;
+                        Console.WriteLine("Please provide valid X,Y values");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Command should be PLACE X,Y,F");
+                    result = false;
+                }
+                if (placecommad[3].Trim().ToUpper() != "NORTH" && placecommad[3].Trim().ToUpper() != "SOUTH"
+                    && placecommad[3].Trim().ToUpper() != "EAST" && placecommad[3].Trim().ToUpper() != "WEST")
+                {
+                    result = false;
+                    Console.WriteLine("Robot should face North, South, East or West");
+                }
+            }
+            else if(inputs.Length == 1)
+            {
+                if (inputs[0].Trim().ToUpper() != "LEFT" && inputs[0].Trim().ToUpper() != "RIGHT" 
+                    && inputs[0].Trim().ToUpper() != "MOVE" && inputs[0].Trim().ToUpper() != "REPORT")
+                {
+                    Console.WriteLine("Command should be Left, Right, Move or Report");
+                    result = false;
+                }
+            }
+            return result;
+        }
+
+        internal static bool validateFirstInput(String input)
+        {
+            String[] inputs = input.Split(",");
+            if(inputs.Length == 3)
+            {
+                return validateInput(input);
+            }
+            else
+            {
+                Console.WriteLine("First Command should be a PLACE command i.e. PLACE X,Y,F");
+                return false;
+            }
+        }
+    }
+}
---
+++ ToyRobotChallenge/ToyRobot.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.AccessControl;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToyRobotChallenge
+{
+    internal class ToyRobot
+    {
+        public int positionX;
+        public int positionY;
+        public String direction = String.Empty;
+        Dictionary<String, String> LeftDic;
+        Dictionary<String, String> RightDic;
+        internal ToyRobot()
+        {
+            LeftDic = new Dictionary<String, String>() { 
+                {"EAST", "NORTH"},
+                {"WEST", "SOUTH"},
+                {"NORTH", "WEST"},
+                {"SOUTH", "EAST"}
+            };
+
+            RightDic = new Dictionary<String, String>()
+            {
+                {"EAST", "SOUTH"},
+                {"WEST", "NORTH"},
+                {"NORTH", "EAST"},
+                {"SOUTH", "WEST"}
+            };     
+        }
+        
+        internal void Command(String input)
+        {
+            String[] inputs = input.Split(",");
+            List<String> command;
+            if (inputs.Length == 3)
+            {
+                command = inputs[0].Split(" ").ToList();
+                command.Add(inputs[1]);
+                command.Add(inputs[2]);
+                Place(command);
+            }
+            else
+            {
+                command = inputs.ToList();
+                switch (command[0].ToUpper())
+                {
+                    case "LEFT":
+                        Left();
+                        break;
+                    case "RIGHT":
+                        Right();
+                        break;
+                    case "MOVE":
+                        Move();
+                        break;
+                    case "REPORT":
+                        Report();
+                        break;
+                }
+            }
+        }
+
+        internal void Place(List<String> command)
+        {
+            positionX = Convert.ToInt32(command[1]);
+            positionY = Convert.ToInt32(command[2]);
+            direction = command[3].Trim().ToUpper();
+        }
+
+        internal void Right()
+        {
+            direction = RightDic[direction];
+            //switch (direction)
+            //{
+            //    case "NORTH":
+            //        direction = "EAST";
+            //        break;
+            //    case "SOUTH":
+            //        direction = "WEST";
+            //        break;
+            //    case "EAST":
+            //        direction = "SOUTH"; 
+            //        break;
+            //    case "WEST":
+            //        direction = "NORTH";
+            //        break;
+            //}
+
+        }
+
+        internal void Left()
+        {
+            direction = LeftDic[direction];
+            //switch (direction)
+            //{
+            //    case "NORTH":
+            //        direction = "WEST";
+            //        break;
+            //    case "SOUTH":
+            //        direction = "EAST";
+            //        break;
+            //    case "EAST":
+            //        direction = "NORTH";
+            //        break;
+            //    case "WEST":
+            //        direction = "SOUTH";
+            //        break;
+            //}
+
+        }
+
+        internal void Move()
+        {
+            switch(direction)
+            {
+                case "NORTH":
+                    if(positionY >= 0 && positionY < 5)
+                        positionY = positionY + 1;
+                    break;
+                case "SOUTH":
+                    if(positionY > 0 && positionY < 5)
+                        positionY = positionY - 1;
+                    break;
+                case "EAST":
+                    if (positionX >= 0 && positionX < 5)
+                        positionX = positionX + 1;
+                    break;
+                case "WEST":
+                    if (positionX > 0 && positionX < 5)
+                        positionX = positionX - 1;
+                    break;
+            }
+        }
+
+        internal void Report()
+        {
+            Console.WriteLine("Current Position: " + positionX + " " + positionY + " " + direction);
+        }
+    }
+}
